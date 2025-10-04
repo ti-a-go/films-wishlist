e6e252bab6a53e6e5a7b526ee246b0096410b98b
Tiago Santos <tiagosn06@gmail.com>
2025-10-03 22:41:55 -0300

    add tests

--- src/auth/auth.guard.spec.ts
+++ src/auth/auth.guard.spec.ts
@@ -1,14 +1,19 @@
 import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { AuthenticationGuard } from './auth.guard';
-import { TestBed } from '@suites/unit';
+import { Mocked, TestBed } from '@suites/unit';
+import { faker } from '@faker-js/faker/.';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthenticationGuard', () => {
   let guard: AuthenticationGuard;
+  let jwtService: Mocked<JwtService>;
 
   beforeEach(async () => {
-    const { unit } = await TestBed.solitary(AuthenticationGuard).compile();
+    const { unit, unitRef } =
+      await TestBed.solitary(AuthenticationGuard).compile();
 
     guard = unit;
+    jwtService = unitRef.get(JwtService);
   });
 
   it('should be defined', () => {
@@ -32,6 +37,64 @@ describe('AuthenticationGuard', () => {
       expect(async () => {
         await guard.canActivate(contextMock as ExecutionContext);
       }).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should throw when JwtService throws', async () => {
+      // Given
+      const token = faker.internet.jwt();
+      const contextMock = {
+        switchToHttp: () => ({
+          getRequest: () => ({
+            headers: {
+              authorization: 'Bearer ' + token,
+            },
+          }),
+        }),
+      };
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      jwtService.verifyAsync.mockImplementationOnce((_token: string) => {
+        return Promise.reject(new Error());
+      });
+
+      // Then
+      expect(async () => {
+        await guard.canActivate(contextMock as ExecutionContext);
+      }).rejects.toThrow(UnauthorizedException);
+      expect(jwtService.verifyAsync).toHaveBeenNthCalledWith(1, token);
+    });
+
+    it('should return true', async () => {
+      // Given
+      const token = faker.internet.jwt();
+      const contextMock = {
+        switchToHttp: () => ({
+          getRequest: () => ({
+            headers: {
+              authorization: 'Bearer ' + token,
+            },
+          }),
+        }),
+      };
+
+      const userPayload = {
+        sub: faker.string.uuid(),
+        username: faker.internet.username(),
+      };
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      jwtService.verifyAsync.mockImplementationOnce((_token: string) => {
+        return Promise.resolve(userPayload);
+      });
+
+      // When
+      const canActivate = await guard.canActivate(
+        contextMock as ExecutionContext,
+      );
+
+      // Then
+      expect(canActivate).toBe(true);
+      expect(jwtService.verifyAsync).toHaveBeenNthCalledWith(1, token);
     });
   });
 });
